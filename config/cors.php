8cd3e85e1e6b0ef064ce7ec4c3bdff075cafc5bd
Hafiz Shahman <hafizshahman1@gmail.com>
2026-03-12 13:15:50 +0800

    Fix CORS configuration to allow vercel domains

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,13 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    'allowed_origins' => [
+        'http://localhost:5173',
+        'http://127.0.0.1:5173',
+        'https://kolekoder.vercel.app',
+        'https://kolekoder.com',
+        'http://kolekoder.com'
+    ],
 
     'allowed_origins_patterns' => [],
 
